0cf87965590c3557f355f29b65717f6fa94e5303
azhangulov-assylbek <azhangulov@gmail.com>
2026-06-16 11:24:41 +0500

    путаница с номерами портов базы данных исправлена

--- database/db.py
+++ database/db.py
@@ -1,14 +1,28 @@
-import psycopg
+# import psycopg
+# import os
+#
+# from psycopg import postgres
+#
+#
+# def get_connection():
+#     return psycopg.connect(
+#         dbname='images_db',
+#         user='postgres',
+#         password='password',
+#         host='localhost',
+#         port='5435'
+#     )
+# верхняя часть кода давала ошибки в docker. пробую с измененным кодом.
 import os
 
-from psycopg import postgres
+import psycopg
 
 
 def get_connection():
     return psycopg.connect(
-        dbname='images_db',
-        user='postgres',
-        password='password',
-        host='localhost',
-        port='5435'
+        dbname=os.getenv('DB_NAME', 'images_db'),
+        user=os.getenv('DB_USER', 'postgres'),
+        password=os.getenv('DB_PASSWORD', 'password'),
+        host=os.getenv('DB_HOST', 'localhost'),
+        port=os.getenv('DB_PORT', '5435'),
     )
